a8513fe8376b6b13e552f133b35c7be7031b6014
Swapnil Raj <raj.swapnil99@gmail.com>
2018-10-09 22:30:43 +0100

    Add function to convert list of pairs to a map

--- markov.hs
+++ markov.hs
@@ -2,9 +2,14 @@ import Data.Map as Map
 
 readCorpus :: IO (Map String [String])
 readCorpus =
-  readFile "speeches.txt" >>= pure . (flip toPairs []) . words
+  readFile "speeches.txt" >>= pure . toMap . (flip toPairs []) . words
+
+combineCorpus :: [a] -> [a] -> [a]
+combineCorpus a b = head a : b
+
+toMap :: Ord a => [(a, [a])] -> Map a [a]
+toMap = Map.fromListWith combineCorpus
 
 toPairs :: [a] -> [(a, [a])] -> [(a, [a])]
 toPairs (x:y:ys) acc = toPairs (y : ys) $ (x, y : []) : acc
 toPairs _ acc = acc
-

d7f6314fe28ae9e1c4dac61110c95e4a48b7d718
Swapnil Raj <raj.swapnil99@gmail.com>
2018-10-09 22:33:12 +0100

    Add initial implementation to generate sentences

--- markov.hs
+++ markov.hs
@@ -13,3 +13,8 @@ toMap = Map.fromListWith combineCorpus
 toPairs :: [a] -> [(a, [a])] -> [(a, [a])]
 toPairs (x:y:ys) acc = toPairs (y : ys) $ (x, y : []) : acc
 toPairs _ acc = acc
+
+generateChain' :: Map String [String] -> Int -> String -> String -> String
+generateChain' _ 0 _ r = r
+generateChain' m n s r = generateChain' m (n-1) (chooseWord) (r ++ " " ++ s)
+  where chooseWord = (m ! s) !! 0

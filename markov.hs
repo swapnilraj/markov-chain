34f1ef8f412b7583f8df95f4b4da134d45c2882f
Swapnil Raj <raj.swapnil99@gmail.com>
2018-10-09 22:29:39 +0100

    Implement reading speeches

--- markov.hs
+++ markov.hs
@@ -1,3 +1,8 @@
+import Data.Map as Map
+
+readCorpus :: IO (Map String [String])
+readCorpus =
+  readFile "speeches.txt" >>= pure . (flip toPairs []) . words
 
 toPairs :: [a] -> [(a, [a])] -> [(a, [a])]
 toPairs (x:y:ys) acc = toPairs (y : ys) $ (x, y : []) : acc

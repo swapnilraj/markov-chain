9f0fc2398ea5a4165dc7d511a721f30606558fe1
Swapnil Raj <raj.swapnil99@gmail.com>
2018-10-09 22:34:37 +0100

    Choose a random word instead of the first word

--- markov.hs
+++ markov.hs
@@ -1,4 +1,5 @@
 import Data.Map as Map
+import System.Random
 
 readCorpus :: IO (Map String [String])
 readCorpus =
@@ -14,7 +15,13 @@ toPairs :: [a] -> [(a, [a])] -> [(a, [a])]
 toPairs (x:y:ys) acc = toPairs (y : ys) $ (x, y : []) : acc
 toPairs _ acc = acc
 
-generateChain' :: Map String [String] -> Int -> String -> String -> String
+generateChain' :: Map String [String] -> Int -> IO String -> IO String -> IO String
 generateChain' _ 0 _ r = r
-generateChain' m n s r = generateChain' m (n-1) (chooseWord) (r ++ " " ++ s)
-  where chooseWord = (m ! s) !! 0
+generateChain' m n s r =
+    r >>= \r' ->
+    s >>= \s' ->
+    generateChain' m (n-1) (chooseRandomWord) $ pure (r' ++ " " ++ s')
+  where
+    wordList = s >>= pure . (m !)
+    chooseRandomWord = wordList >>= \wordList' ->
+      randomRIO (0, length wordList' - 1) >>= pure . (wordList' !!)

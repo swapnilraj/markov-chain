cca97078e16150c42765c0040c572e614adf9d61
Swapnil Raj <raj.swapnil99@gmail.com>
2019-05-10 21:04:44 +0100

    Setup cabal project

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
+module Main where
+
 import Data.Map as Map
 import System.Random
 
 readCorpus :: IO (Map String [String])
-readCorpus =
-  readFile "speeches.txt" >>= pure . toMap . (flip toPairs []) . words
+readCorpus = toMap . flip toPairs [] . words <$> readFile "speeches.txt"
 
 combineCorpus :: [a] -> [a] -> [a]
 combineCorpus a b = head a : b
@@ -12,25 +13,23 @@ toMap :: Ord a => [(a, [a])] -> Map a [a]
 toMap = Map.fromListWith combineCorpus
 
 toPairs :: [a] -> [(a, [a])] -> [(a, [a])]
-toPairs (x:y:ys) acc = toPairs (y : ys) $ (x, y : []) : acc
+toPairs (x:y:ys) acc = toPairs (y : ys) $ (,) x [y] : acc
 toPairs _ acc = acc
 
-generateChain' :: Map String [String] -> Int -> IO String -> IO String -> IO String
+generateChain' :: Map String [String] -> Int -> String -> String -> String
 generateChain' _ 0 _ r = r
 generateChain' m n s r =
-    r >>= \r' ->
-    s >>= \s' ->
-    generateChain' m (n-1) (chooseRandomWord) $ pure (r' ++ " " ++ s')
+    generateChain' m (n-1) (chooseRandomWord) $ r ++ " " ++ s
   where
     wordList = s >>= pure . (m !)
     chooseRandomWord = wordList >>= \wordList' ->
-      randomRIO (0, length wordList' - 1) >>= pure . (wordList' !!)
+      randomRIO (0, length wordList' - 1) >>= (wordList' !!)
 
-generateChain :: Map String [String] -> Int -> String -> IO String
-generateChain m n s = generateChain' m n (pure s) (pure "")
+generateChain :: Map String [String] -> Int -> String -> String
+generateChain m n s = generateChain' m n s mempty
 
 main =
   putStrLn "Hey what word should the speech start with?" >>
     getLine >>= \word -> putStrLn "How many words do you want it to be?" >>
       getLine >>= \len -> readCorpus >>=
-        \map -> generateChain map (read len :: Int) word >>= print
+        \map -> generateChain map (read len) word >>= print
